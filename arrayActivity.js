5268da7e2dcdde46674e671e93aef5157627e543
santchaycabel <santchaycabel18@gmail.com>
2025-03-26 21:42:43 +0800

    arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,17 @@
+// Create an array called favSingers and store three favorite singers
+let favSingers = ["Moira Dela Torre", "Ben&Ben", "Taylor Swift"];
+
+// Log the first singer in the array
+console.log("My first favorite singer:", favSingers[0]);
+
+// Create an array called favNumbers and store four favorite numbers
+let favNumbers = [10, 13, 20, 22];
+
+// Create an array named mixedArr with different data types
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+// Access and log each item in the mixedArr using array index notation
+console.log("First item in mixedArr:", mixedArr[0]);
+console.log("Second item in mixedArr:", mixedArr[1]);
+console.log("Third item in mixedArr:", mixedArr[2]);
+console.log("Fourth item in mixedArr:", mixedArr[3]);
